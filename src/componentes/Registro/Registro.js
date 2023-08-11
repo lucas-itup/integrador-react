ba8b2ab853096b7fcbc0f890501d25c624a14f82
Lucas25 <lucas@it-up.com.ar>
2023-08-11 13:04:03 -0300

    last

--- src/componentes/Registro/Registro.js
+++ src/componentes/Registro/Registro.js
@@ -33,7 +33,7 @@ export const Registro = () => {
             }
         } catch (error) {
             console.error('Error de Registro:', error.response ? error.response.data : error.message);
-            setError('Error de Registro: ' + error.response ? error.response.data : error.message);
+            setError('Credenciales incorrectas. Por favor, verifica tu nombre de usuario y contraseña.');
             // Aquí puedes mostrar un mensaje de error al usuario si lo deseas
         }
     };
